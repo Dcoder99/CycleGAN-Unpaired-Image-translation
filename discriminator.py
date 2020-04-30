b57037f0e073a3d20b35ab9534212d9fef98f8a8
Deepika Karanji <deeps.karanji2@gmail.com>
2020-04-30 15:26:57 +0530

    loss mae batch=1

--- discriminator.py
+++ discriminator.py
@@ -8,7 +8,7 @@ def convLayer(input, k, slope=0.2, stride=2, reuse=False, is_training=True, name
       initializer=tf.random_normal_initializer(mean=0.0, stddev=0.02, dtype=tf.float32))
     conv = tf.nn.conv2d(input, W_var, strides=[1, stride, stride, 1], padding='SAME')
     normalized = instance_norm(conv)
-    output = tf.maximum(slope*normalized, normalized)###########leakyRelu
+    output = tf.maximum(slope*normalized, normalized)#leakyRelu
     return output
 
 
@@ -34,15 +34,15 @@ class Discriminator:
   def __call__(self, input):
     with tf.variable_scope(self.name):
       C64 = convLayer(input, 64, reuse=self.reuse,
-          is_training=self.is_training, name='C64')             # (?, w/2, h/2, 64)
+          is_training=self.is_training, name='C64')             
       C128 = convLayer(C64, 128, reuse=self.reuse,
-          is_training=self.is_training, name='C128')            # (?, w/4, h/4, 128)
+          is_training=self.is_training, name='C128')            
       C256 = convLayer(C128, 256, reuse=self.reuse,
-          is_training=self.is_training, name='C256')            # (?, w/8, h/8, 256)
+          is_training=self.is_training, name='C256')          
       C512 = convLayer(C256, 512,reuse=self.reuse,
-          is_training=self.is_training, name='C512')            # (?, w/16, h/16, 512)
+          is_training=self.is_training, name='C512')  
 
-      output = lastLayer(C512, reuse=self.reuse, name='output')          # (?, w/16, h/16, 1)
+      output = lastLayer(C512, reuse=self.reuse, name='output')
 
     self.reuse = True
     self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)
@@ -53,9 +53,8 @@ class Discriminator:
 def instance_norm(input):
   with tf.variable_scope("instance_norm"):
     depth = input.get_shape()[3]
-    # scale = _weights("scale", [depth], mean=1.0)
-    scale = tf.get_variable("scale", [depth], initializer=tf.random_normal_initializer(mean=1.0, stddev=0.02, dtype=tf.float32))
-    # offset = _biases("offset", [depth])
+    scale = tf.get_variable("scale", [depth], \
+      initializer=tf.random_normal_initializer(mean=1.0, stddev=0.02, dtype=tf.float32))
     offset = tf.get_variable("offset", [depth], initializer=tf.constant_initializer(0.0))
     mean, variance = tf.nn.moments(input, axes=[1,2], keep_dims=True)
     epsilon = 1e-5
